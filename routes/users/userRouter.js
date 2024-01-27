544506a5dbae036cd433ad420af7df75258b3a13
Muhammad Hicho A Haidor <mh.haidor@gmail.com>
2024-01-27 16:48:04 +0100

    put user done

--- routes/users/userRouter.js
+++ routes/users/userRouter.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const userSchema = require("../../models/userSchema");
 const loginUser = require("../login/loginRouter");
-const verifyToken = require("../../Auth/AuthJwtMW");
-const authorize = require("../../Auth/AutorizationMW");
+const verifyToken = require("../middlewares/auth/AuthJwtMW");
+const authorize = require("../middlewares/auth/AutorizationMW");
 const schemaRegister = require("../../validations/registerSchema");
 const { rojo, verde, print } = require("../../helpers/colors");
+const bcrypt = require("bcrypt");
+const checkUserExistence = require("../middlewares/userExists");
 
 router.use("/login", loginUser);
 
@@ -218,26 +220,32 @@ router.get("/:email", async (req, res) => {
  *               type: string
  *               description: Mensaje de error general.
  */
-router.post("/", async (req, res) => {
+//Añadir nuevo usuario
+router.post("/", checkUserExistence, async (req, res) => {
   // validate user
   const { error } = schemaRegister.validate(req.body);
 
+  console.log(error);
   if (error) {
     console.log({ error: error.details[0].message });
     return res.status(400).json({ error: error.details[0].message });
   }
 
-  const isEmailExist = await userSchema.findOne({ email: req.body.email });
-  if (isEmailExist) {
-    return res.status(400).json({ error: "Email ya registrado" });
-  }
+  //const isEmailExist = await userSchema.findOne({ email: req.body.email });
+  //if (isEmailExist) {
+  //  return res.status(400).json({ error: "Email ya registrado" });
+  //}
+
+  // Hash de la contraseña
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
 
   //Por defecto al registrarse el rol es user y el array de reservas está vacío
 
   const data = new userSchema({
     user_name: req.body.user_name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
     role: "user",
     reservations: [],
   });
@@ -253,21 +261,110 @@ router.post("/", async (req, res) => {
 });
 
 //modificar un usuario
-router.patch("/update", async (req, res) => {
+/**
+ * @swagger
+ * /api/users/update/{email}:
+ *   patch:
+ *     summary: Actualizar usuario
+ *     description: Actualiza la información de un usuario existente en la base de datos.
+ *     tags: [Usuarios]
+ *     parameters:
+ *       - in: path
+ *         name: email
+ *         required: true
+ *         description: Correo electrónico del usuario a actualizar.
+ *         schema:
+ *           type: string
+ *           format: email
+ *     requestBody:
+ *       description: Campos a actualizar del usuario.
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               user_name:
+ *                 type: string
+ *                 description: Nuevo nombre de usuario.
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: Nuevo correo electrónico del usuario.
+ *               password:
+ *                 type: string
+ *                 description: Nueva contraseña del usuario.
+ *               reservations:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     room_number:
+ *                       type: number
+ *                       description: Número de la habitación de la reserva.
+ *                     check_in_date:
+ *                       type: string
+ *                       format: date
+ *                       description: Fecha de entrada de la reserva.
+ *                     check_out_date:
+ *                       type: string
+ *                       format: date
+ *                       description: Fecha de salida de la reserva.
+ *                 description: Lista de nuevas reservas del usuario.
+ *     responses:
+ *       '200':
+ *         description: Documento actualizado exitosamente.
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: Documento actualizado exitosamente
+ *       '400':
+ *         description: Error en la solicitud o en la actualización del documento.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Detalles del error
+ *               message: Mensaje de error
+ *       '404':
+ *         description: Usuario no encontrado en la base de datos.
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: Documento no encontrado
+ *       '500':
+ *         description: Error interno del servidor.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Detalles del error
+ *               message: Mensaje de error
+ */
+//modificar usuario
+router.patch("/update/:email", checkUserExistence, async (req, res) => {
   try {
-    const email = req.body.email;
+    const email = req.params.email;
 
-    const resultado = await userSchema.updateOne(
+    // Solo actualizamos los campos que se envían en el cuerpo de la solicitud
+    const updateFields = {
+      user_name: req.body.user_name,
+      email: req.body.email,
+      password: req.body.password,
+      reservations: req.body.reservations,
+    };
+
+    // Si se proporciona un nuevo password, lo hasheamos
+    if (req.body.password) {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      updateFields.password = hashedPassword;
+    }
+
+     // Eliminamos campos undefined del objeto para que no se sobrescriban con valores undefined
+     Object.keys(updateFields).forEach(key => updateFields[key] === undefined && delete updateFields[key]);
+
+     console.log(updateFields);
+     const resultado = await userSchema.updateOne(
       { email },
-      {
-        $set: {
-          user_name: req.body.user_name,
-          email: req.body.email,
-          password: req.body.password,
-          role: req.body.role,
-          reservations: req.body.reservations,
-        },
-      }
+      { $set: updateFields }
     );
 
     if (resultado.modifiedCount === 0) {
